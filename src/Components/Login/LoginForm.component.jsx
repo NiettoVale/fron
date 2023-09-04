faaba49886aa79e79e022f9d66436d0fa0678193
NiettoVale <nietto.program@gmail.com>
2023-09-04 19:51:49 -0300

    ESPERO QUE AHORA FUNCIONE TODO LPM

--- src/Components/Login/LoginForm.component.jsx
+++ src/Components/Login/LoginForm.component.jsx
@@ -21,13 +21,16 @@ const LoginForm = () => {
   const handleSubmit = async (event) => {
     event.preventDefault();
     try {
-      const response = await fetch("http://localhost:3001/login", {
-        method: "POST",
-        headers: {
-          "Content-Type": "application/json",
-        },
-        body: JSON.stringify(formData),
-      });
+      const response = await fetch(
+        "https://espacioflipante.onrender.com/login",
+        {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify(formData),
+        }
+      );
 
       const responseData = await response.json();
 
